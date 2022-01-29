c76b24800c4c4c796c0bd1731804f37f080f9f20
JHaley1513 <34946424+JHaley1513@users.noreply.github.com>
2022-01-29 17:02:37 -0500

    Defined NotesApp model, updated .gitignore

--- backend/notesapp/models.py
+++ backend/notesapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class NotesApp(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField() # not initialized yet
+    # completed: status of a task, always is either completed or not.
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
