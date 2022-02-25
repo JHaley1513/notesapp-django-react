d327e0006aa20d6600d4a8a35b543f7f715b43f2
JHaley1513 <34946424+JHaley1513@users.noreply.github.com>
2022-02-24 21:05:42 -0500

    Installed Redux pkgs, added Reducer and Root Store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Root from "./Root";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
@@ -10,14 +11,14 @@ class App extends Component {
     return (
       <div>
         {/* Any text here will be displayed on all pages routed below */}
-        <BrowserRouter>
+        <Root>
           <Switch>
             <Route path="/signup" component={Signup} />
             <Route path="/login" component={Login} />
             <Route path="/dashboard" component={Dashboard} />
             <Route exact path="/" component={Home} />
           </Switch>
-        </BrowserRouter>
+        </Root>
       </div>
     );
   }
